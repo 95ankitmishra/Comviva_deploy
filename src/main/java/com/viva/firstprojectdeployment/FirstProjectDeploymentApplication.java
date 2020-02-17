383804f86c5206b5abb263e9264521f7fc07dba5
95ankitmishra <40460402+95ankitmishra@users.noreply.github.com>
2020-02-17 23:18:56 +0530

    second

---
+++ src/main/java/com/viva/firstprojectdeployment/FirstProjectDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.viva.firstprojectdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viva/firstprojectdeployment/FirstProjectDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viva.firstprojectdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
